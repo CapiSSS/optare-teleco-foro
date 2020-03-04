54112287f746d2462eb8b9bb2bc8374766538acf
Sergio Sancho Sanz <ssancho@optaresolutions.com>
2020-03-04 09:08:57 +0100

    Change date string format

--- client/js/correo.js
+++ client/js/correo.js
@@ -6,4 +6,11 @@ respuestas += "'statuscode':'" + sessionStorage.getItem('statuscode') + "',"
 respuestas += "'gb':'" + sessionStorage.getItem('gb') + "'}"
 document.getElementById("responses").value = respuestas
 var d = new Date()
-document.getElementById("fecha").value = d.toUTCString()
+document.getElementById("fecha").value = "" + d.getFullYear() + "-" + appendLeadingZeroes(d.getMonth() + 1) + "-" + appendLeadingZeroes(d.getDate()) + " " + appendLeadingZeroes(d.getHours()) + ":" + appendLeadingZeroes(d.getMinutes()) + ":" + appendLeadingZeroes(d.getSeconds())
+
+function appendLeadingZeroes(n){
+  if(n <= 9){
+    return "0" + n;
+  }
+  return n
+}
